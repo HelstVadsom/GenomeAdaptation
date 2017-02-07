deb242311a0a79f098874919afb4fea0bd78a0d2
Helst Vadsom <forsberg.timmy@live.com>
2017-02-07 13:31:08 +0100

    Add files via upload
    Basic Set up 0.1

---
+++ LoadData.py
@@ -0,0 +1,8 @@
+#from numpy import *
+
+# mutational probability
+data1 = loadtxt("probDeleteriousMut.txt") # dtype 'f64'
+
+data2 = loadtxt("correspGeneID.dat",dtype = 'S9')
+
+# effects sizes
---
+++ Main.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy import *
+
+execfile("LoadData.py")
+
+execfile("InitilizeParameters.py")
+
+data1 = data2 = None # delete unneccesary variables.
+
+
+
+
+
+
