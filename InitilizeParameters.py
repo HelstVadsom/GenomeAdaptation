d686e86c675e64d12b3edea3f70befef19fcbd96
Helst Vadsom <forsberg.timmy@live.com>
2017-02-10 13:23:13 +0100

    Update InitilizeParameters.py

--- InitilizeParameters.py
+++ InitilizeParameters.py
@@ -7,50 +7,49 @@ DTYPE2 = 'uint8'
 MINIMUM_TIME_RESOLUTION = .1 # [min] (0.1 min = 6 sek) min: ~6 otherwise change float16 @ CreateInd...
 
 
-# CONSTANT:
+## CONSTANT:
 # scalars
-FOUNDER_COUNT = 10^5 # Nr. initial agents.
-
-SAMPLE_COUNT = 10^5 # Nr. sampled agents for each cycle
-
-YIELD = 5 # Nr. population doublings.
-
+NR_ENVIRONMENTS = 8
 NR_CYCLES = 50
 
-LAG_TIME_MEAN = 90 # [min]
+FOUNDER_COUNT = 10**5 # Nr. initial agents.
+SAMPLE_COUNT = 10**5 # Nr. sampled agents for each cycle
 
-CELL_CYCLE_TIME_MEAN = 90 # [min]
+YIELD = 5 # Nr. population doublings.
+MAXIMUM_NR_AGENTS = FOUNDER_COUNT*2**YIELD # per cycle
+
+MEAN_LAG_TIME = 90 # [min]
+MEAN_CELL_CYCLE_TIME = 90 # [min]
 
 GENOME_LENGTH = 5 # Nr. of possible mutations. max: 9, otherwise change uint8 @ CreateInd...
 
-MAXIMUM_NR_AGENTS = FOUNDER_COUNT*2^YIELD # per cycle
 
 # vectors
-LAG_TIMES = LAG_TIME_MEAN + np.random.normal(0, 1, [FOUNDER_COUNT, 1])
+LAG_TIMES = MEAN_LAG_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
 
-CELL_CYCLE_TIMES = CELL_CYCLE_TIME_MEAN + np.random.normal(0, 1, [FOUNDER_COUNT, 1])
+CELL_CYCLE_TIMES = MEAN_CELL_CYCLE_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
 
-FOUNDER_ID = np.array([range(FOUNDER_COUNT)]).T # max: 4,294,967,295
+FOUNDER_ID = range(FOUNDER_COUNT) #np.array(range(FOUNDER_COUNT)).T # max: 4,294,967,295
 
 #MUTATION_PROB = data1[range(5)]
 
 #MUTATION_CELL_CYCLE_EFFECT = [0.5, 1.2
 
 
-# CHANGEABLE:
+## CHANGEABLE:
 # vectors
-lag_progress = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = DTYPE1)
+lag_progress = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
 
-lag_time = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = DTYPE1)
+lag_time = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
 lag_time[0:FOUNDER_COUNT] = LAG_TIMES
 
-cell_cycle_time = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = DTYPE1)
+cell_cycle_time = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
 cell_cycle_time[0:FOUNDER_COUNT] = CELL_CYCLE_TIMES
 
-mutation = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = DTYPE2)
+mutation = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE2)
 
-founder_ID = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = "uint32")
-founder_ID[0:FOUNDER_COUNT] = FOUNDER_ID
+founder_id = np.zeros([MAXIMUM_NR_AGENTS],dtype = "uint32")
+founder_id[0:FOUNDER_COUNT] = FOUNDER_ID
 
-age = np.zeros([MAXIMUM_NR_AGENTS,1],dtype = DTYPE1) # unsure Dtype
+age = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1) # unsure Dtype
 
