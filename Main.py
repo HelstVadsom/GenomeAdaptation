a835c3f08c5f784aac4b8474fcd26b1384371571
Helst Vadsom <forsberg.timmy@live.com>
2017-02-10 13:06:09 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,12 +3,16 @@ from numpy import *
 
 execfile("LoadData.py")
 
+# \todo: HERE, Decide which mutations to look at.
+
 execfile("InitilizeParameters.py")
 
-data1 = data2 = None # delete unneccesary variables.
+execfile("CreateIndividuals.py")
 
+# \todo: HERE, Delete variables we don't need anymore.
+#data1 = data2 = None # delete unneccesary variables.
 
-
-
-
-
+for iEnv in xrange(NR_ENVIRONMENTS):
+    for iCycle in xrange(NR_CYCLES):
+        print 'iEnv = ', iEnv,'iCycle = ',iCycle
+        
