1b974550a89c16e28177af0c71a7d9e5d15dcff6
Helst Vadsom <forsberg.timmy@live.com>
2017-02-10 13:37:39 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,14 +13,19 @@ execfile("CreateIndividuals.py")
 #data1 = data2 = None # delete unneccesary variables.
 
 for iEnv in xrange(NR_ENVIRONMENTS):
-    if __name__ == '__main__':
-        for iCycle in xrange(NR_CYCLES):
-            print 'iEnv = ', iEnv,'iCycle = ',iCycle
+    for iCycle in xrange(NR_CYCLES):
+        print 'iEnv = ', iEnv, 'iCycle = ', iCycle
+        stillInCycle = 1
+        while stillInCycle:
             # \todo: Make time tic
             # \todo: check exit lag, (and if no cells in lag, turn off check)
             # \todo: check time to divide
+            # \todo: adjust time step
             # \todo: do mutate, in both or just ONE? calculate new ccycle time etc...
             # \todo: divide: set age, founder id
             # \todo: add born individuals to existing ones
-            # \todo: check cycle exit condition. break
-    
+            # \todo: check cycle exit condition. stillInCycle = 0
+        # \todo: save importants
+    # \todo: save importants
+# \todo: generate plots,
+# \todo: make the code
