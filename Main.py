07b58cd69599238f7126ac1e217690f86b8d4d13
Helst Vadsom <forsberg.timmy@live.com>
2017-02-13 13:37:15 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,16 +20,31 @@ for iEnv in xrange(NR_ENVIRONMENTS):
         stillInCycle = 1#1
         while stillInCycle:
             print 'time is ', t
-            # \todo: Make time tic
-            # \todo: check exit lag, (and if no cells in lag, turn off check)
-            if sum(simEnv['lag_time'] > t) != 0:
-                alive_n_lag = (simEnv['lag_time'] <= t).dot(simEnv['age'] >= 1)
+            # Make time tic
+            t += MINIMUM_TIME_RESOLUTION # Possible speed up? find biggest time increment...
+            # Check exit lag, (and if no cells in lag, turn off check)
+            if any(simEnv['lag_time'][0:nr_alive] > t):
+                alive_n_lag = (simEnv['lag_time'][0:nr_alive] <= t).dot(simEnv['age'][0:nr_alive] >= 1)
                 simEnv['lag_progress'][alive_n_lag] = 1
+                #t += 1
             # \todo: check time to divide
+            # Find dividing agents
+            toDivide = simEnv['cell_cycle_time'][0:nr_alive] <= t) #.dot(simEnv['age'][0:nr_alive] >= 1)
+            # Find mutations
+            toMutate = np.random.random([len(toDivide,1)]) < PROB_MUTATION
+            if any(toMutate):
+                for iMutate in nonzero(toMutate)):
+                    mutatedORF = floor(np.random.random() * (sum(data1) + 1))
+                    
+
+
             # \todo: adjust time step
             # \todo: do mutate, in both or just ONE? calculate new ccycle time etc...
             # \todo: divide: set age, founder id
             # \todo: add born individuals to existing ones
+            # Population ages
+            nr_alive += 1 # expand to batch
+            simEnv['age'][0:nr_alive] += 0.1
             # \todo: check cycle exit condition. stillInCycle = 0
         # \todo: Sample for next cycle
         # \todo: save importants
