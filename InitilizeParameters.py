612511599eb5e306b37d5a5ac089c5f1c08c268a
Helst Vadsom <forsberg.timmy@live.com>
2017-02-13 13:38:11 +0100

    Update InitilizeParameters.py

--- InitilizeParameters.py
+++ InitilizeParameters.py
@@ -6,29 +6,26 @@ DTYPE1 = 'float16'
 DTYPE2 = 'uint8'
 MINIMUM_TIME_RESOLUTION = .1 # [min] (0.1 min = 6 sek) min: ~6 otherwise change float16 @ CreateInd...
 
+MUTATION_RATE = 0.33*10**-9 # per base, per division.
+GENOME_SIZE = 1.3*10**7 # nr. of bases
+ORF_SIZE = sum(data3)
+PROB_MUTATION = MUTATION_RATE * ORF_SIZE
 
 ## CONSTANT:
 # scalars
-NR_ENVIRONMENTS = 8
-NR_CYCLES = 50
-
+NR_ENVIRONMENTS = 1#8
+NR_CYCLES = 1#50
 FOUNDER_COUNT = 10**5 # Nr. initial agents.
 SAMPLE_COUNT = 10**5 # Nr. sampled agents for each cycle
-
 YIELD = 5 # Nr. population doublings.
 MAXIMUM_NR_AGENTS = FOUNDER_COUNT*2**YIELD # per cycle
-
 MEAN_LAG_TIME = 90 # [min]
 MEAN_CELL_CYCLE_TIME = 90 # [min]
-
 GENOME_LENGTH = 5 # Nr. of possible mutations. max: 9, otherwise change uint8 @ CreateInd...
-
 
 # vectors
 LAG_TIMES = MEAN_LAG_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
-
 CELL_CYCLE_TIMES = MEAN_CELL_CYCLE_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
-
 FOUNDER_ID = range(FOUNDER_COUNT) #np.array(range(FOUNDER_COUNT)).T # max: 4,294,967,295
 
 #MUTATION_PROB = data1[range(5)]
@@ -37,6 +34,9 @@ FOUNDER_ID = range(FOUNDER_COUNT) #np.array(range(FOUNDER_COUNT)).T # max: 4,294
 
 
 ## CHANGEABLE:
+# scalars
+nr_alive = FOUNDER_COUNT
+
 # vectors
 lag_progress = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
 
@@ -52,4 +52,4 @@ founder_id = np.zeros([MAXIMUM_NR_AGENTS],dtype = "uint32")
 founder_id[0:FOUNDER_COUNT] = FOUNDER_ID
 
 age = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1) # unsure Dtype
-
+age[0:FOUNDER_COUNT] = 1 # Born
