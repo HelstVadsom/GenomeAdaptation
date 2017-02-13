e5c6b9b8269ebc697829be3389f7a8184d6f5810
Helst Vadsom <forsberg.timmy@live.com>
2017-02-13 13:37:48 +0100

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -3,6 +3,8 @@
 # mutational probability
 data1 = loadtxt("nrLOFMut.txt") # dtype 'f64'
 
+data1_Cum = np.cumsum(data1)
+
 data2 = loadtxt("LOF_ORFs.dat",dtype = 'S9')
 
 data3 = loadtxt("ORF_Length") # dtype 'f64'
