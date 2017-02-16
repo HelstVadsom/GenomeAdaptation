03ae6605b3c1d7a5bc1391a8afe4649bb0c4e8b6
Helst Vadsom <forsberg.timmy@live.com>
2017-02-16 14:33:08 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,28 +17,32 @@ for iEnv in xrange(NR_ENVIRONMENTS):
     for iCycle in xrange(NR_CYCLES):
         print 'iEnv = ', iEnv, 'iCycle = ', iCycle
         t = 0
-        stillInCycle = 1#1
+        stillInCycle = 1 # 1
         while stillInCycle:
             print 'time is ', t
-            # Make time tic
             t += MINIMUM_TIME_RESOLUTION # Possible speed up? find biggest time increment...
             # Check exit lag, (and if no cells in lag, turn off check)
             if any(simEnv['lag_time'][0:nr_alive] > t):
                 alive_n_lag = (simEnv['lag_time'][0:nr_alive] <= t).dot(simEnv['age'][0:nr_alive] >= 1)
                 simEnv['lag_progress'][alive_n_lag] = 1
-                #t += 1
-            # \todo: check time to divide
-            # Find dividing agents
-            toDivide = simEnv['cell_cycle_time'][0:nr_alive] <= t) #.dot(simEnv['age'][0:nr_alive] >= 1)
-            # Find mutations
-            toMutate = np.random.random([len(toDivide,1)]) < PROB_MUTATION
+
+            ## Divide
+            toDivide = (simEnv['cell_cycle_time'][0:nr_alive] <= t) #.dot(simEnv['age'][0:nr_alive] >= 1)
+            #print simEnv[18,19]
+            toBirth = np.arange(nr_alive,(nr_alive + len(toDivide)))
+            #simEnv[toBirth] = simEnv[toDivide] # transcribe properties
+
+            ## Mutate
+            toMutate = np.random.random([len(toDivide),1]) < PROB_MUTATION
             if any(toMutate):
-                for iMutate in nonzero(toMutate)):
-                    mutatedORF = floor(np.random.random() * (sum(data1) + 1))
-                    
-
-
-            # \todo: adjust time step
+                idxMutate = nonzero(toMutate) # Which individuals dividing that mutate.
+                mutateORF = np.random.random([sum(toMutate),1]) * (data1_Cum[-1]) # pick mutations
+                idxORF = np.searchsorted(data1_Cum, mutateORF, side='right') # see which mutation it was
+                # \todo: check if that mutation has already happend.
+                # calculate mutation impact on cell cycle time
+                simEnv['cell_cycle_time'][idxMutate[0]] = simEnv['cell_cycle_time'][idxMutate[0]] + \
+                                                          data4[idxORF,iEnv].T*(CELL_CYCLE_TIMES[simEnv['founder_id'][idxMutate[0]]])
+            nr_alive += 1
             # \todo: do mutate, in both or just ONE? calculate new ccycle time etc...
             # \todo: divide: set age, founder id
             # \todo: add born individuals to existing ones
@@ -51,4 +55,3 @@ for iEnv in xrange(NR_ENVIRONMENTS):
     # \todo: save importants
 # \todo: generate plots,
 # \todo: make the code faster.
-
