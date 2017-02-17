e25f5ec584bb985be18b84957159612264b6e576
Helst Vadsom <forsberg.timmy@live.com>
2017-02-17 16:41:25 +0100

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -1,12 +1,14 @@
-#from numpy import *
+import numpy as np
 
 # mutational probability
-data1 = loadtxt("nrLOFMut.txt") # dtype 'f64'
+data1 = np.loadtxt("nrLOFMut.txt") # dtype 'f64'
 
 data1_Cum = np.cumsum(data1)
 
-data2 = loadtxt("LOF_ORFs.dat",dtype = 'S9')
+data2 = np.loadtxt("LOF_ORFs.dat",dtype = 'S9')
 
-data3 = loadtxt("ORF_Length") # dtype 'f64'
+data3 = np.loadtxt("ORF_Length") # dtype 'f64'
 
 # effects sizes
+
+data4 = np.loadtxt("mmGT.dat")
