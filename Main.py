05c5c401735ab12df0720222f609865586b6d37f
Helst Vadsom <forsberg.timmy@live.com>
2017-02-23 13:42:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -30,20 +30,18 @@ def run():
         while still_in_cycle:
 
             t += MINIMUM_TIME_RESOLUTION # Possible speed up? find biggest time increment...
-            
+
             # Check lag
             if lag:
-                lagging = (sim_env['lag_time'][:nr_alive] > 0) #.dot(sim_env['age'][0:nr_alive] >= 1)
+                lagging = (sim_env['lag_time'][:nr_alive] > 0)
                 lag = any(lagging)
                 sim_env['lag_time'][lagging] -= MINIMUM_TIME_RESOLUTION
                 sim_env['next_divition'][lagging] += MINIMUM_TIME_RESOLUTION
 
             ## Divide
-            to_divide = (sim_env['next_divition'][:nr_alive] <= t)#.dot(sim_env['lag_time'][0:nr_alive] >= t)
+            to_divide = (sim_env['next_divition'][:nr_alive] <= t)
             nr_divide = np.sum(to_divide)
             if nr_divide:
-                # print nr_divide
-                # print np.nonzero(to_divide)[0]
                 divide_index = np.nonzero(to_divide)[0]
                 to_birth = np.arange(nr_alive,(nr_alive + nr_divide))
                 sim_env['founder_id'][to_birth] = divide_index
@@ -51,7 +49,6 @@ def run():
                 sim_env['cell_cycle_time'][to_birth] = sim_env['cell_cycle_time'][to_divide]
                 sim_env['next_divition'][to_divide] = t + sim_env['cell_cycle_time'][to_divide]
                 sim_env['next_divition'][to_birth] = sim_env['next_divition'][to_divide]
-                # CELL_CYCLE_TIMES[sim_env['founder_id'][to_divide]]
                 sim_env['nr_divitions'][to_divide] += 1
 
                 ## Mutate
@@ -59,9 +56,8 @@ def run():
                 if any(to_mutate):
                     mutate_orf = np.random.random([sum(to_mutate), 1]) * (data1_Cum[-1]) # pick mutation
                     orf_index = np.searchsorted(data1_Cum, mutate_orf, side='right') # peek mutation
-                    mutation[to_birth[to_mutate]] = orf_index  # store mutation
+                    # mutation[to_birth[to_mutate]] = orf_index  # store mutation
                     # divide_index = np.nonzero(to_divide)[0]
-                    # nr_mutations +=
                     print 'time is ', t
                     print 'alive: ', nr_alive
                     print gen_time[orf_index, i_env]
@@ -78,6 +74,7 @@ def run():
 
             if nr_alive >= MAXIMUM_NR_AGENTS - 10:
                 still_in_cycle = 0
+                print 'Exiting cycle: ', i_cycle
 
             # Save data for Growth curve
             if t % 20 == 0:
@@ -88,17 +85,16 @@ def run():
                 # nr unmutated cells,
                 # time of mutation
 
-        # \todo: Save data for histograms of GT
+        # Save data for histograms of GT
+        gt_cycle[:MAXIMUM_NR_AGENTS, i_cycle] = (sim_env['cell_cycle_time'] - MEAN_CELL_CYCLE_TIME) / MEAN_CELL_CYCLE_TIME
 
-
-        # Save meanGT at end of cycles.
+        # Save meanGT
         meanGT[i_cycle] = np.mean(sim_env['cell_cycle_time'][:MAXIMUM_NR_AGENTS])
 
         # Sample for next cycle
         sample = np.random.choice(MAXIMUM_NR_AGENTS, SAMPLE_COUNT,replace=False)
         sim_env[:SAMPLE_COUNT] = sim_env[sample]
         nr_alive = SAMPLE_COUNT
-        #sim_env['next_divition'][:nr_alive] = sim_env['cell_cycle_time'][:nr_alive]
 
         # \todo: save importants
     # \todo: save importants
@@ -108,13 +104,7 @@ def run():
 
 
 
-def getGT(i,fc): # calculates the effective GT of agent i, fc should be FOUNDER_COUNT or SAMPLE_COUNT
-    if sim_env['founder_id'][i] < fc:
-        return gen_time[sim_env['mutation'][i], i_env]
-    else:
-        return getGT(sim_env['founder_id'][i],fc) +  gen_time[sim_env['mutation'][i], i_env]
-
 
 if __name__ == "__main__":
-    i_env = 1
+    i_env = 0
     run()
