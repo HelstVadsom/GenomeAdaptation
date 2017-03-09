793c3cf41248c4a48afad0993065a5c9ad6efc6b
Helst Vadsom <forsberg.timmy@live.com>
2017-03-09 16:09:03 +0100

    Update CreateIndividuals.py

--- CreateIndividuals.py
+++ CreateIndividuals.py
@@ -4,14 +4,14 @@ from InitilizeConstants import *
 DTYPE1 = 'float16'
 DTYPE2 = 'uint8'
 
-lag_time                         = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
-lag_time[0:FOUNDER_COUNT]        = LAG_TIMES
-cell_cycle_time                  = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
-cell_cycle_time[0:FOUNDER_COUNT] = CELL_CYCLE_TIMES
-founder_id                       = np.zeros([MAXIMUM_NR_AGENTS],dtype = "uint32")
-founder_id[0:FOUNDER_COUNT]      = FOUNDER_ID
-age                              = np.zeros([MAXIMUM_NR_AGENTS],dtype = 'float64')
-nr_divisions                     = np.zeros([MAXIMUM_NR_AGENTS],dtype = DTYPE1)
+lag_time                        = np.zeros([MAXIMUM_NR_AGENTS], dtype=DTYPE1)
+lag_time[:FOUNDER_COUNT]        = LAG_TIMES
+cell_cycle_time                 = np.zeros([MAXIMUM_NR_AGENTS], dtype=DTYPE1)
+cell_cycle_time[:FOUNDER_COUNT] = CELL_CYCLE_TIMES
+founder_id                      = np.zeros([MAXIMUM_NR_AGENTS], dtype="uint32")
+founder_id[:FOUNDER_COUNT]      = FOUNDER_ID
+age                             = np.zeros([MAXIMUM_NR_AGENTS], dtype='float64')
+nr_divisions                    = np.zeros([MAXIMUM_NR_AGENTS], dtype=DTYPE1)
 
 # pre-allocate
 sim_env = np.zeros(MAXIMUM_NR_AGENTS, dtype={'names':['lag_time', 'cell_cycle_time', 'next_division_time',
@@ -26,4 +26,4 @@ sim_env['founder_id'] = founder_id
 sim_env['age'] = age
 sim_env['nr_divisions'] = nr_divisions
 
-mutation =  - np.ones([MAXIMUM_NR_AGENTS,10], dtype = 'int16')
+mutation =  - np.ones([MAXIMUM_NR_AGENTS, 5], dtype = 'int16')
