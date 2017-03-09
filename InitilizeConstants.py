6306106f2acf1af268bda0381bc78c7e664aed1e
Helst Vadsom <forsberg.timmy@live.com>
2017-03-09 16:08:30 +0100

    Update InitilizeConstants.py

--- InitilizeConstants.py
+++ InitilizeConstants.py
@@ -3,14 +3,15 @@ import numpy as np
 from LoadData import orf_target_size_cums #, data3
 
 # GENERAL
-MUTATION_RATE           = 0.33*10**-9 # per base, per division.
+MUTATION_RATE           = 0.33*10**-9  # per base, per division.
 MUTATION_PROB           = MUTATION_RATE * orf_target_size_cums[-1]
+OVERLAP_ALLOWED         = True  # Is > 1 mutation per orf for each agent allowed? (False recommended)
 
 ## CONSTANT:
 # scalars
 NR_CYCLES               = 50
 FOUNDER_COUNT           = 10**5 # Nr. initial agents.
-SAMPLE_COUNT            = 10**5 # Nr. sampled agents for each cycle
+SAMPLE_COUNT            = 10**5 # Nr. sampled agents after each cycle
 YIELD                   = 5 # Nr. population doublings.
 MAXIMUM_NR_AGENTS       = FOUNDER_COUNT*2**YIELD # per cycle,
 MEAN_LAG_TIME           = 90 # [min]
