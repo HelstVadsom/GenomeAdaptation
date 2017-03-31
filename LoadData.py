511de64faef528a0be2a272fc68fc210094d07cd
Helst Vadsom <forsberg.timmy@live.com>
2017-03-31 15:01:53 +0200

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # mutational probability
-orf_target_size = np.loadtxt("nStopGainMutationsPerBase.dat") + \
-                  np.loadtxt("nDeleriousMutationsPerBase.dat")
+orf_target_size = np.loadtxt("nStopGainBaseMutationsPerORF.dat") + \
+                  np.loadtxt("nDeleriousBaseMutationsPerORF.dat")
 
 orf_target_size_cums = np.cumsum(orf_target_size)
 
@@ -10,4 +10,4 @@ orf_target_size_cums = np.cumsum(orf_target_size)
 orfs = np.loadtxt("uniqueORF2.txt",dtype = 'S9')
 
 # effects sizes
-gen_time = np.loadtxt("GT_Env.dat") # change in GT for each LOF mutation for each environment.
+gen_time = np.loadtxt("GT_Env.dat") # change in GT for each LOF 
