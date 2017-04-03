bf0ceb1d9bad9d6af84479e31b5caa2e0dc7c955
Helst Vadsom <forsberg.timmy@live.com>
2017-04-03 12:08:21 +0200

    Update InitilizeConstants.py

--- InitilizeConstants.py
+++ InitilizeConstants.py
@@ -3,20 +3,23 @@ import numpy as np
 from LoadData import orf_target_size_cums #, data3
 
 # GENERAL
-MUTATION_RATE           = 0.33*10**-9  # per base, per division.
-MUTATION_PROB           = MUTATION_RATE * orf_target_size_cums[-1]
+MUTATION_RATE           = 0 #0.33*10**-9  # per base, per division.
+MUTATION_PROB           = MUTATION_RATE * orf_target_size_cums[-1] / 3
 OVERLAP_ALLOWED         = True  # Is > 1 mutation per orf for each agent allowed? (False recommended)
+TIME_STEP               = 20  #[min]
 
-## CONSTANT:
+# CONSTANT:
 # scalars
 NR_CYCLES               = 50
 FOUNDER_COUNT           = 10**5 # Nr. initial agents.
 SAMPLE_COUNT            = 10**5 # Nr. sampled agents after each cycle
-YIELD                   = 5 # Nr. population doublings.
+YIELD                   = 5 # Nr. of allowed population doublings.
 MAXIMUM_NR_AGENTS       = FOUNDER_COUNT*2**YIELD # per cycle,
-MEAN_LAG_TIME           = 90 # [min]
+MAXIMUM_NR_DIVISIONS    = 1000
+MEAN_LAG_TIME           = 90 # [min]  # HAppens in the beginning of each cycle. Cells will not further its cell cycle while in lag.
 MEAN_CELL_CYCLE_TIME    = 90 # [min]
-EXPERIMENT_TIME         = 72*60 # [min]
+EXPERIMENT_TIME         = 1 #72*60 # [min]
+
 
 # vectors
 LAG_TIMES               = MEAN_LAG_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
