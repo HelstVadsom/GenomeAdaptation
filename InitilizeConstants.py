edc85567a583ab86610f8c16fb5db8a889ba63f6
Helst Vadsom <forsberg.timmy@live.com>
2017-05-03 11:36:35 +0200

    Update InitilizeConstants.py

--- InitilizeConstants.py
+++ InitilizeConstants.py
@@ -10,18 +10,18 @@ TIME_STEP               = 20  #[min]
 
 # CONSTANT:
 # scalars
-NR_CYCLES               = 50
+NR_CYCLES               = 20
 FOUNDER_COUNT           = 10**5 # Nr. initial agents.
 SAMPLE_COUNT            = 10**5 # Nr. sampled agents after each cycle
 YIELD                   = 5 # Nr. of allowed population doublings.
 MAXIMUM_NR_AGENTS       = FOUNDER_COUNT*2**YIELD # per cycle,
 MAXIMUM_NR_DIVISIONS    = 1000
-MEAN_LAG_TIME           = 804.8 # [min]  # Happens in the beginning of each cycle. Cells will not further its cell cycle while in lag.
-MEAN_CELL_CYCLE_TIME    = 162.3 # 90 # [min]
+MEAN_LAG_TIME           = 90 # [min]  # Happens in the beginning of each cycle. Cells will not further its cell cycle while in lag.
+MEAN_CELL_CYCLE_TIME    = 90 # [min]
 EXPERIMENT_TIME         = 1 #72*60 # [min]
 
 
 # vectors
-LAG_TIMES               = MEAN_LAG_TIME * np.ones([FOUNDER_COUNT]) #+ #np.random.normal(0, 1, [FOUNDER_COUNT])
-CELL_CYCLE_TIMES        = MEAN_CELL_CYCLE_TIME * np.ones([FOUNDER_COUNT]) #+ np.random.normal(0, 1, [FOUNDER_COUNT])
+LAG_TIMES               = MEAN_LAG_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
+CELL_CYCLE_TIMES        = MEAN_CELL_CYCLE_TIME + np.random.normal(0, 1, [FOUNDER_COUNT])
 FOUNDER_ID              = range(FOUNDER_COUNT)
